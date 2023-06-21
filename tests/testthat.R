880e881de5334882a2a0e580538e7f0bc1dc635d
Patrick Chester <pjc468@nyu.edu>
2023-06-21 13:35:17 -0700

    Edit testthat.R

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,8 +1,4 @@
 library("testthat")
 library("conclust")
-# library("keras")
-# use_session_with_seed(42)
-# see https://github.com/rstudio/keras/issues/890#issuecomment-539044011
-# tensorflow::tf$random$set_seed(42)
 
 test_check("conclust")
