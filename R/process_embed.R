5a32a5987b06d8968f2749b5e92f2226eeaffd46
Patrick Chester <pjc468@nyu.edu>
2023-08-25 14:45:13 -0700

    Create process_embed function

---
+++ R/process_embed.R
@@ -0,0 +1,35 @@
+#' A tool designed to reduce redundant terms in a fitted embedding model
+#'
+#' @description Takes a fitted embedding model as an input. Allows users to combine
+#' embeddings by the case, stem, or lemma of associated terms.
+#' @param x A fitted word embedding model in the data frame format
+#' @param words The name of a column that corresponds to the word dimension of the fitted word embeddings
+#' @param punct Removes punctuation
+#' @param tolower Combines terms that differ by case
+#' @param lemmatize Combines terms that share a common lemma. Uses the lexicon package by default.
+#' @param stem Combines terms that share a common stem. *Note:* Stemming should not be used in conjunction with lemmatize.
+#' @keywords conclust
+#' @import textstem
+#' @import data.table
+#' @export
+process_embed <- function(x, words = NULL, punct = TRUE, tolower = TRUE, lemmatize = TRUE, stem = FALSE) {
+    UseMethod("process_embed")
+}
+
+#' @export
+process_embed.data.frame <- function(x, words = NULL, punct = TRUE, tolower = TRUE, lemmatize = TRUE, stem = FALSE) {
+  if(!(words %in% names(x))) {stop("Term column name not found.\\n")}
+  if(tolower) {x[[words]] <- tolower(x[[words]])}
+  if(lemmatize) {x[[words]] <- lemmatize_words(x[[words]])}
+  if(stem) {x[[words]] <- stem_words(x[[words]])}
+  numeric_logical_cols <- sapply(x, is.numeric) | sapply(x, is.logical)
+  x <- as.data.table(x)
+  if(punct) {
+    punct_rows <- grepl(pattern = "[[:punct:]]", x[[words]])
+    x <- x[!punct_rows, ]
+    }
+  x <- x[, lapply(.SD, mean), by = eval(words), .SDcols = numeric_logical_cols]
+  x <- x[, lapply(.SD, mean), by = eval(words), .SDcols = numeric_logical_cols]
+  x <- as.data.frame(x)
+  return(x)
+}
